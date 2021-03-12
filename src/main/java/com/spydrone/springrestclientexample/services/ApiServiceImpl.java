b17a71d44caad810fd4f5faeccc259672b8bd671
Michael Morrison <morri909@umn.edu>
2021-03-12 09:36:49 -0600

    Refactor api url to prop

--- src/main/java/com/spydrone/springrestclientexample/services/ApiServiceImpl.java
+++ src/main/java/com/spydrone/springrestclientexample/services/ApiServiceImpl.java
@@ -1,6 +1,7 @@
 package com.spydrone.springrestclientexample.services;
 
 import com.spydrone.springrestclientexample.api.domain.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,14 +13,16 @@ import java.util.List;
 public class ApiServiceImpl implements ApiService {
 
 	private final RestTemplate restTemplate;
+	private final String apiUri;
 
-	public ApiServiceImpl(RestTemplate restTemplate) {
+	public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.uri}") String apiUri) {
 		this.restTemplate = restTemplate;
+		this.apiUri = apiUri;
 	}
 
 	@Override
 	public List<User> getUsers() {
-		ResponseEntity<User[]> response = restTemplate.getForEntity("https://api.mocki.io/v1/b043df5a", User[].class);
+		ResponseEntity<User[]> response = restTemplate.getForEntity(apiUri, User[].class);
 		return Arrays.asList(response.getBody());
 	}
 }
