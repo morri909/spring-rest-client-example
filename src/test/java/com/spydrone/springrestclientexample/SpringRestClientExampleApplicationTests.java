39c841a1bc33ad6ca92cd44377a2d23fcfdfde13
Michael Morrison <morri909@umn.edu>
2021-03-11 19:25:34 -0600

    init commit

---
+++ src/test/java/com/spydrone/springrestclientexample/SpringRestClientExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spydrone.springrestclientexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestClientExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
