069f19874cde3f79eb5d72b6e98fd81311002525
Michael Morrison <morri909@umn.edu>
2021-03-11 19:29:15 -0600

    Create data model

---
+++ src/main/java/com/spydrone/springrestclientexample/api/domain/User.java
@@ -0,0 +1,33 @@
+package com.spydrone.springrestclientexample.api.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	private String gender;
+	private Name name;
+	private Location location;
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Name getName() {
+		return name;
+	}
+
+	public void setName(Name name) {
+		this.name = name;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+}
